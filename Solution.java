a9f475f0a68e58057b04c49474e936e7373b7ee1
nanicharan23 <55834015+nanicharan23@users.noreply.github.com>
2021-09-18 22:15:50 +0530

    Update Solution.java
    Added a for loop

--- Solution.java
+++ Solution.java
@@ -1,7 +1,10 @@
 class Solution{
 	public static void main(String args[]){
 		int a=100;
+		int c=500;
 		int b=200;
+		for(int i=0;i<100;i++)
+			System.out.println(i);
 		System.out.println(a+b);
 	}
 }
