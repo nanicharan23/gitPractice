fc0d3851071ed1bf732afcf3da7485869426fe6b
sri_charan <sri_charan@persistent.com>
2021-11-12 22:53:17 +0530

    changed for loop because mistake found

--- Solution.java
+++ Solution.java
@@ -3,7 +3,7 @@ class Solution{
 		int a=100;
 		int c=500;
 		int b=200;
-		for(int i=0;i<100;isdfasdf++)
+		for(int i=0;i<100;i++)
 			System.out.println(i);
 		System.out.println(a+b);
 		System.out.println("This is the change made from sricharna232000 account");
